277a9a99e83e5f32f46015074704dd91eb8b6c1b
Chris Raczynski <raczynski.k89@gmail.com>
2022-05-21 17:08:13 -0500

    Adding main package files: init, lowflow

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+This module uses Fisher-Jenks natural breakpoint analysis for identifying 
+objective breakpoint threshold of low flows.
+
+This method is further described in:
+Raczynski K., Dyer J., 2022, Development of an Objective Low Flow Identification
+Method Using Breakpoint Analysis, XXX(X), XXX-XXX. doi:XXX-XXX-XX-XX-X
+
+Future functionalities will be added in later development.
+
+@authors: Raczynski Krzysztof, Dyer Jamie
+"""
+
+__version__ = '0.1'
+
+__all__ = ['threshold', 'multiple']
